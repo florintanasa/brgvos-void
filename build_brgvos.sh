3e05df61f7162989d87bd42dff31d0eefce09e5a
Florin Tanasa <florin.tanasa@gmail.com>
2025-07-10 22:14:40 +0300

    Enhances ISO build script
    Updates the ISO build script to generate SHA256 checksum files alongside the ISO images. This ensures the integrity of the created ISO file. Also corrects a flag duplication in the `mkiso.sh` call and standardizes ISO file naming.

--- build_brgvos.sh
+++ build_brgvos.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+
 # set the date and time
 data=$(date +'%d%m%Y_%H%M%S')
+
 # change the owner for includedir
 chown root:root -R includedir
+
 # change working directory
 cd void-mklive
+
 # flag used for build the iso
 arch=$(cat ../arch)
 variant=$(cat ../variant)
@@ -15,18 +19,24 @@ linux_version=$(cat ../linux_version)
 title=$(cat ../title)
 kernel_arg=$(cat ../kernel_arg)
 other_pkg=$(cat ../other_pkg)
+
 # run void linux script to build iso file image with our distribution
 sudo ./mkiso.sh \
 -a $arch \
 -b $variant \
 -- -k $keymap \
+-B $variant \
 -l $locale \
 -e $root_shell \
 -v $linux_version \
 -T $title \
 -C "$kernel_arg" \
 -p "$other_pkg" \
--o $title'_'$variant'_'$locale'_'$arch'_'$data'.iso' \
+-o $title'_'$variant'_'$locale'_'$arch'_'$data.iso \
 -I ../includedir
+
+#create hash file
+HASH=`sha256sum $title'_'$variant'_'$locale'_'$arch'_'$data.iso`
+echo $HASH > $title'_'$variant'_'$locale'_'$arch'_'$data.sha256
 
 sync
