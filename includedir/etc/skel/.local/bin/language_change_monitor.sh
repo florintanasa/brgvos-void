d2c8e9bc7b76fe8a3f843f56f740f0c0e4f639f7
Florin Tanasa <florin.tanasa@gmail.com>
2026-01-24 14:06:11 +0200

    Sets user language at first login in Gnome
    Ensures the user's language is correctly set in Gnome during the first login.
    
    It uses the locale setting initially, and if the language is English, it explicitly sets the Gnome language to English.
    
    For other languages, it calls the `on_language_change` function to ensure proper language configuration.
    
    Creates a file to prevent repeated execution on subsequent logins.

--- includedir/etc/skel/.local/bin/language_change_monitor.sh
+++ includedir/etc/skel/.local/bin/language_change_monitor.sh
@@ -9,14 +9,10 @@ get_language() {
   org.freedesktop.Accounts.User Language | awk -F "'" '{print $2}'
 }
 
-# Print the current language
-CURRENT_LANGUAGE=$(get_language)
-echo "Current Language: $CURRENT_LANGUAGE"
-
 # Function to handle the language change
 on_language_change() {
     local new_language="$1"
-    #echo "Language changed to: $new_language"
+    echo "Language changed to: $new_language"
     case "$CURRENT_LANGUAGE" in
         de_DE.UTF-8|es_ES.UTF-8|fr_FR.UTF-8|it_IT.UTF-8|pt_BR.UTF-8|pt_PT.UTF-8|ro_RO.UTF-8|ru_RU.UTF-8|zh_TE.UTF-8) set_"$CURRENT_LANGUAGE"_gnome.sh 4;;
     esac
@@ -25,8 +21,47 @@ on_language_change() {
         en_US.UTF-8) set_"$CURRENT_LANGUAGE"_gnome.sh 4;;
     esac
     CURRENT_LANGUAGE=$new_language
-    #echo "New current language is $CURRENT_LANGUAGE"
+    echo "New current language is $CURRENT_LANGUAGE"
 }
+
+# Get the current language, at first login from locale and then using D-Bus call
+# using Accounts service for the user.
+if [ ! -e $HOME/.set_locale_gnome ]
+    then
+        CURRENT_LANGUAGE=$(locale | grep LANG | awk -F "=" '{print $2}')
+        echo "Current Language (using locale): $CURRENT_LANGUAGE"
+        # If the user have English current language so also the system is in English,
+        # I set also in Gnome English language because the language was not set for the Gnome
+        if [ "$CURRENT_LANGUAGE" == "en_US.UTF-8" ]; then
+            gdbus call --system \
+            --dest org.freedesktop.Accounts \
+            --object-path /org/freedesktop/Accounts/User$(id -u) \
+            --method org.freedesktop.Accounts.User.SetLanguage "en_US.UTF-8"
+        else
+            # Call funtion with parameter to be sure the language is set for the user,
+            # in case was not set when the user was created,
+            # in this case the language in Gnome is set with system language. 
+            on_language_change "$CURRENT_LANGUAGE"
+        fi
+        # Create the file .set_locale_gnome
+        touch $HOME/.set_locale_gnome
+        # Write the message in file .set_locale_gnome
+        cat << 'EOF' > $HOME/.set_locale_gnome
+        ############################################################
+        ###                                                      ###
+        ### This file was created by next script:                ###
+        ### %HOME/.local/bin/language_change_monitor.sh          ###
+        ### and is created only once at first login in Gnome.    ###
+        ### If you delete this file is created again.            ###
+        ###                                                      ###
+        ############################################################
+
+EOF
+    else
+        CURRENT_LANGUAGE=$(get_language)
+        echo "Current Language (using D-BUS): $CURRENT_LANGUAGE"
+
+fi
 
 # Listen for changes in user language
 gdbus monitor -y -d org.freedesktop.Accounts -o "/org/freedesktop/Accounts/User$(id -u)" |
