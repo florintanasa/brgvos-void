923162e7ada2744c5f9f01f4f6dfb91bb413e79d
Florin Tanasa <florin.tanasa@gmail.com>
2026-01-24 17:33:01 +0200

    fix: Correct zh_TE.UTF-8 to zh_TW.UTF-8 in language monitor

--- includedir/etc/skel/.local/bin/language_change_monitor.sh
+++ includedir/etc/skel/.local/bin/language_change_monitor.sh
@@ -14,10 +14,10 @@ on_language_change() {
     local new_language="$1"
     echo "Language changed to: $new_language"
     case "$CURRENT_LANGUAGE" in
-        de_DE.UTF-8|es_ES.UTF-8|fr_FR.UTF-8|it_IT.UTF-8|pt_BR.UTF-8|pt_PT.UTF-8|ro_RO.UTF-8|ru_RU.UTF-8|zh_TE.UTF-8) set_"$CURRENT_LANGUAGE"_gnome.sh 4;;
+        de_DE.UTF-8|es_ES.UTF-8|fr_FR.UTF-8|it_IT.UTF-8|pt_BR.UTF-8|pt_PT.UTF-8|ro_RO.UTF-8|ru_RU.UTF-8|zh_TW.UTF-8) set_"$CURRENT_LANGUAGE"_gnome.sh 4;;
     esac
     case "$new_language" in
-        de_DE.UTF-8|es_ES.UTF-8|fr_FR.UTF-8|it_IT.UTF-8|pt_BR.UTF-8|pt_PT.UTF-8|ro_RO.UTF-8|ru_RU.UTF-8|zh_TE.UTF-8) set_"$new_language"_gnome.sh 2;;
+        de_DE.UTF-8|es_ES.UTF-8|fr_FR.UTF-8|it_IT.UTF-8|pt_BR.UTF-8|pt_PT.UTF-8|ro_RO.UTF-8|ru_RU.UTF-8|zh_TW.UTF-8) set_"$new_language"_gnome.sh 2;;
         en_US.UTF-8) set_"$CURRENT_LANGUAGE"_gnome.sh 4;;
     esac
     CURRENT_LANGUAGE=$new_language
