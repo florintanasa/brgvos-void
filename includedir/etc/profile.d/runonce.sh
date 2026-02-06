c8ad27402f020d4b8cdd840c7092278ac1ff0405
Florin Tanasa <florin.tanasa@gmail.com>
2026-02-06 17:40:37 +0200

    profile.d: Improve apparmor-notify autostart management

--- includedir/etc/profile.d/runonce.sh
+++ includedir/etc/profile.d/runonce.sh
@@ -24,9 +24,22 @@ then
 EOF
     # add install.desktop for user brgos only because after install I delete file install.desktop from system
     if [ -e /usr/local/share/applications/install.desktop ]; then
-	mkdir -p $HOME/Desktop
-	cp /usr/local/share/applications/install.desktop $HOME/Desktop
-    # do not autostart for brgvos user apparmor-notify.desktop
-    sed -i 's/X-GNOME-Autostart-enabled=true/X-GNOME-Autostart-enabled=false/g' $HOME/.config/autostart/apparmor-notify.desktop
+	    mkdir -p $HOME/Desktop
+	    cp /usr/local/share/applications/install.desktop $HOME/Desktop
+        # do not autostart for brgvos user apparmor-notify.desktop
+        sed -i 's/X-GNOME-Autostart-enabled=true/X-GNOME-Autostart-enabled=false/g' $HOME/.config/autostart/apparmor-notify.desktop
+    fi 
+    # Disable if audit or apparmor are not installed, maybe wish to install later the missing package
+    if ! xbps-query -S audit >/dev/null 2>&1 || ! xbps-query -S apparmor >/dev/null 2>&1; then
+        if [ -f "$HOME/.config/autostart/apparmor-notify.desktop" ]; then
+            sed -i 's/X-GNOME-Autostart-enabled=true/X-GNOME-Autostart-enabled=false/g' "$HOME/.config/autostart/apparmor-notify.desktop"
+        fi
+    fi
+    # Delete if audit and apparmor are not installed
+    if ! xbps-query -S audit >/dev/null 2>&1 && ! xbps-query -S apparmor >/dev/null 2>&1; then
+        if [ -f "$HOME/.config/autostart/apparmor-notify.desktop" ]; then
+            rm -f "$HOME/.config/autostart/apparmor-notify.desktop"
+            rm -f "$HOME/.local/bin/run-aa-notify.sh"
+        fi
     fi
 fi
