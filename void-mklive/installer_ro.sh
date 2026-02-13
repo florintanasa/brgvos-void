0034236c7fc034c9a7343a024e10e27b66574116
Florin Tanasa <florin.tanasa@gmail.com>
2026-02-13 20:17:52 +0200

    feat(installer_ro): Enhance audit configuration and localize messages

--- void-mklive/installer_ro.sh
+++ void-mklive/installer_ro.sh
@@ -1087,33 +1087,37 @@ set_audit() {
   _user=$(get_option USERLOGIN)
   {
     if cat "$TARGETDIR"/etc/group | grep -q "audit"; then
-      echo "Group 'audit' exist, so is not created..."
+      echo "Grupul 'audit' există, deci nu este creat..."
     else
-      echo "Create group 'audit'..."
+      echo "Creez grupul 'audit'..."
       # Create group audit
       chroot "$TARGETDIR" groupadd -r audit
     fi
     # Add user to audit group
     chroot "$TARGETDIR" gpasswd -a "$_user" audit
     if cat "$TARGETDIR"/etc/audit/auditd.conf | grep -q "log_group = root"; then
-      echo "Change log_group to audit..."
+      echo "Schimb log_group = audit..."
       # Change log_group to audit
       chroot "$TARGETDIR" sed -i 's/log_group = root/log_group = audit/g' /etc/audit/auditd.conf
     fi
     if cat "$TARGETDIR"/usr/lib/tmpfiles.d/audit.conf | grep -q "d /var/log/audit 0700 root root - -"; then
-      echo "Change file mode and group for directory /var/log/audit..."
+      echo "Schimb permisiunea fieșierului și a grupului pentru directorul /var/log/audit..."
       # Change file mode and group for directory /var/log/audit
       chroot "$TARGETDIR" sed -i 's/d \/var\/log\/audit 0700 root root - -/d \/var\/log\/audit 0750 root audit - -/g'  /usr/lib/tmpfiles.d/audit.conf
     fi
-    echo "Enable service auditctl to start at boot..."
+    echo "Activez serviciul auditctl să pornească la boot..."
     chroot "$TARGETDIR" ln -s /etc/sv/auditctl /etc/runit/runsvdir/default/
-    echo "Enable service auditd to start at boot..."
+    echo "Activez serviciul auditd să pornească la boot..."
     chroot "$TARGETDIR" ln -s /etc/sv/auditd /etc/runit/runsvdir/default/
     # Copy rules file from /tmp to $TARGET/tmp, then copy rules file to /etc/audit/rules.d
     if [ -f /tmp/99-myconfig.rules ]; then
+      echo "Copii fișierul cu reguli din /tmp în $TARGET/tmp, după care copii fișierul în /etc/audit/rules.d"
       cp /tmp/99-myconfig.rules "$TARGETDIR"/tmp
       chroot "$TARGETDIR" cp /tmp/99-myconfig.rules /etc/audit/rules.d/
     fi
+    echo "Introduc -i (Ignorare erori) în /etc/audit/rules.d/10-base-config.rules"
+    chroot "$TARGETDIR" sed -i '$a# Ignore errors' /etc/audit/rules.d/10-base-config.rules
+    chroot "$TARGETDIR" sed -i '$a-i' /etc/audit/rules.d/10-base-config.rules
   } >>$LOG 2>&1
 }
 
