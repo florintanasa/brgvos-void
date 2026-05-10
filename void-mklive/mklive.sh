e502060c64b6030f8b485f7215ec015c041f6521
Florin Tanasa <florin.tanasa@gmail.com>
2026-05-10 12:40:09 +0300

    Fix for the last dracut update

--- void-mklive/mklive.sh
+++ void-mklive/mklive.sh
@@ -221,7 +221,7 @@ generate_initramfs() {
 
     copy_dracut_files "$ROOTFS"
     copy_autoinstaller_files "$ROOTFS"
-    chroot "$ROOTFS" env -i /usr/bin/dracut -N --"${INITRAMFS_COMPRESSION}" \
+    chroot "$ROOTFS" env -i PATH="/usr/bin:/usr/sbin:/bin:/sbin" /usr/bin/dracut -N --"${INITRAMFS_COMPRESSION}" \
         --add-drivers "ahci" --force-add "vmklive autoinstaller" --omit systemd "/boot/initrd" $KERNELVERSION
     [ $? -ne 0 ] && die "Failed to generate the initramfs"
 
