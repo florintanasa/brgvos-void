c6484d96a15b23c92bba19eb1c107710f9101fa9
Florin Tanasa <florin.tanasa@gmail.com>
2026-09-02 08:15:06 +0300

    Update: added the exact file

--- files/includedir/brgvos-repo-x86_64-current.sh
+++ files/includedir/brgvos-repo-x86_64-current.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to build archive
-tar -czf brgvos-repo-x86_64-current.tar.gz usr/share/xbps.d/*
+tar -czf brgvos-repo-x86_64-current.tar.gz usr/share/xbps.d/01-brgv-os-current.conf
